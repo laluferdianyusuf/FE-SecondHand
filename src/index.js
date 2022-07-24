da9eda3910605c77b420562164329aa79dce1912
Ferdian Yusuf <lferdianyusuf@gmail.com>
2022-07-25 01:55:18 +0800

    Commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-router-dom";
+import { render } from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
